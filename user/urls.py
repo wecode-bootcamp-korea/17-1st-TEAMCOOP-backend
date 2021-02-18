f4b5f341304ce0fa102c1db9793cfe8c0138fb4b
hyeyoonc <lucy8624@naver.com>
2021-02-18 16:07:59 +0900

    Create: Login View

---
+++ user/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from user.views  import LoginView
+
+urlpatterns = [
+        path('/login', LoginView.as_view()),
+        ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
+import bcrypt
+import jwt
 
-# Create your views here.
+from django.http        import JsonResponse, HttpResponse
+from django.views       import View
+from django.db.models   import Q
+
+from my_settings  import SECRET_KEY, ALGORITHM
+from user.models  import User
+
+
+class LoginView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            email    = data['email']
+            password = data['password']
+        
+            if User.objects.filter(email=email).exists():
+                user = User.objects.get(email=email)
+            
+                if bcrypt.checkpw(password.encode('utf-8'),user.password.encode('utf-8')):
+                    token = jwt.encode({'user':user.id}, SECRET_KEY,ALGORITHM)
+                    return JsonResponse({"MESSAGE":"SUCCESS", "ACCESS_TOKEN":token}, status = 200)
+                return JsonResponse({"MESSAGE":"INVALID_PASSWORD"}, status = 401)
+            return JsonResponse({"MESSAGE":"INVALID_USER"}, status = 401)
+        
+        except KeyError:
+            return JsonResponse({"MESSAGE":"KEY_ERROR"}, status = 400)
