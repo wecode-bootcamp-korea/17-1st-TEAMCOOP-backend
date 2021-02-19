d6b18ace8c8971e0381a6fabdd16c2ae01a9f6cf
ScottieKim <fmychioh@gmail.com>
2021-02-19 15:36:47 +0900

    Create: Sms 인증번호 전송 View
    SmsView - 인증번호 전송 View. NaverCloud SMS API사용
    
    SmsValidationView  - 인증번호 Authentication View

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views      import SignupView, LoginView
+from .views      import SignupView, LoginView, SmsSendView, SmsValidationView
 
 urlpatterns = [
     path('/signup', SignupView.as_view()),
     path('/login', LoginView.as_view()),
+    path('/sms', SmsSendView.as_view()),
+    path('/sms-validation', SmsValidationView.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -2,17 +2,23 @@ import re
 import json
 import bcrypt
 import jwt
+import time
+import hashlib
+import hmac
+import base64
+import requests
 
-from django.http import JsonResponse, HttpResponse
+from random       import randint
+
+from django.http  import JsonResponse, HttpResponse
 from django.views import View
-from django.db.models import Q
+from django.utils import timezone
 
-from my_settings import SECRET_KEY, ALGORITHM
-from user.models import User
+from my_settings  import SECRET_KEY, ALGORITHM, SMS
+from user.models  import User, AuthNumber
 
 
 MINIMUM_PASSWORD_LENGTH = 8
-
 
 def validate_email(email):
     pattern = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
@@ -20,23 +26,14 @@ def validate_email(email):
         return False
     return True
 
-
-def validate_phone_number(phone_number):
-    pattern = re.compile('^[0]\d{2}\d{3,4}\d{4}$')
-    if not pattern.match(phone_number):
-        return False
-    return True
-
-
 class SignupView(View):
     def post(self, request):
         try:
-            signup_data = json.loads(request.body)
-            name = signup_data['name']
-            email = signup_data['email']
-            confirm = signup_data['confirm']
-            phone_number = signup_data['number']
-            password = signup_data['password']
+            signup_data     = json.loads(request.body)
+            name            = signup_data['name']
+            email           = signup_data['email']
+            phone_number    = signup_data['number']
+            password        = signup_data['password']
 
             if User.objects.filter(email=email).exists():
                 return JsonResponse({"message": "USER_ALREADY_EXISTS"}, status=400)
@@ -47,18 +44,14 @@ class SignupView(View):
             if not validate_email(email):
                 return JsonResponse({"message": "EMAIL_VALIDATION_ERROR"}, status=400)
 
-            if not validate_phone_number(phone_number):
-                return JsonResponse({"message": "PHONENUMBER_VALIDATION_ERROR"}, status=400)
-
-            hased_pw = bcrypt.hashpw(
-                signup_data['password'].encode('utf-8'), bcrypt.gensalt())
+            hased_pw          = bcrypt.hashpw(signup_data['password'].encode('utf-8'), bcrypt.gensalt())
             decoded_hashed_pw = hased_pw.decode('utf-8')
 
             User.objects.create(
-                name=name,
-                email=email,
-                phone_number=phone_number,
-                password=decoded_hashed_pw
+                name          = name,
+                email         = email,
+                phone_number  = phone_number,
+                password      = decoded_hashed_pw
             )
 
             return JsonResponse({"message": "SUCCESS"}, status=200)
@@ -66,12 +59,92 @@ class SignupView(View):
         except KeyError:
             return JsonResponse({"message": "KeyError"}, status=400)
 
+class SmsSendView(View):
+    def validate_phone_number(self, phone_number):
+        pattern = re.compile('^[0]\d{2}\d{3,4}\d{4}$')
+        if not pattern.match(phone_number):
+            return False
+        return True
+
+    def make_signature(self, string):
+        secret_key    = bytes(SMS['secret_key'], 'UTF-8')
+        string        = bytes(string, 'UTF-8')
+        string_hmac   = hmac.new(secret_key, string, digestmod=hashlib.sha256).digest()
+        string_base64 = base64.b64encode(string_hmac).decode('UTF-8')
+        return string_base64
+
+    def post(self, request):   
+        try:
+            data         = json.loads(request.body)
+            phone_number = data['phone_number']
+            
+            if not self.validate_phone_number(phone_number=phone_number):
+                return JsonResponse({'message':"PHONE_NUMBER_VALIDATION_ERROR"}) 
+
+            url            = "https://sens.apigw.ntruss.com/sms/v2/services/" + SMS['service_id'] + "/messages"
+            uri            = "/sms/v2/services/" + SMS['service_id'] + "/messages"
+            timestamp      = str(int(time.time() * 1000))
+            access_key     = SMS['access_key']
+            string_to_sign = "POST " + uri + "\n" + timestamp + "\n" + access_key
+            signature      = self.make_signature(string=string_to_sign)
+            auth_number    = randint(1000,10000)
+            headers        = { 
+                "Content-Type"            : "application/json",
+                "x-ncp-apigw-timestamp"	  : timestamp,
+                'x-ncp-iam-access-key'    : access_key,
+                'x-ncp-apigw-signature-v2': signature
+            }
+
+            body           = {
+                "type": "SMS",
+                "from": SMS['from_number'],
+                "messages":[{"to":phone_number}],
+                "content": "[coreof] Please enter [{}].".format(auth_number)  
+            }
+
+            body          = json.dumps(body)
+            response      = requests.post(url, headers=headers, data=body)
+            response_dict = response.json()
+            status_code   = response_dict['statusCode'] if 'statusCode' in response_dict else response_dict['status']
+
+            if int(status_code) != 202:
+                return JsonResponse({"message": "SMS_SEND_FAIL"}, status=400)
+                
+            AuthNumber.objects.update_or_create(
+                phone_number = phone_number,
+                defaults     = {
+                    'auth_number': auth_number
+                }
+            )
+            return JsonResponse({"message": "SUCCESS"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"message":"KEY_ERROR"}, status=400)
+
+class SmsValidationView(View):
+    def post(self, request):
+        try:
+            data         = json.loads(request.body)
+            phone_number = data['phone_number']
+            auth_number  = int(data['auth_number'])
+            user_auth    = AuthNumber.objects.get(phone_number=phone_number)
+            
+            if auth_number != user_auth.auth_number:
+                return JsonResponse({"message":"WRONG_CODE"}, status=400)
+
+            if (timezone.now() - user_auth.updated_at).seconds >= 60:
+                return JsonResponse({"message":"TIMES_UP"}, status=400)
+
+            return JsonResponse({"message":"SUCCESS"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"message":"KEY_ERROR"}, status=400)
 
 class LoginView(View):
     def post(self, request):
         try:
-            data = json.loads(request.body)
-            email = data['email']
+            data     = json.loads(request.body)
+            email    = data['email']
             password = data['password']
 
             if User.objects.filter(email=email).exists():
