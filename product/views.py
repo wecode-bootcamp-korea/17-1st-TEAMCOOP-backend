b37bbe5c828a31c559455f3317ad90796fc274d5
hyeyoonc <lucy8624@naver.com>
2021-02-23 02:21:07 +0900

    "Add: ProductView 신상품 보는 뷰"

--- product/views.py
+++ product/views.py
@@ -70,3 +70,32 @@ class ProductView(View):
                 result.append(context)
 
             return JsonResponse({"data": result, "message": "SUCCESS"}, status=200)
+        
+        #recently added  
+        if id == 99999:            
+            products = Product.objects.filter(is_new=True)
+            for product in products:
+                goals_product = product.goal.all()
+                product_SSPs  = product.productstock_set.all()
+
+                product_price_list = [product_SSP.price for product_SSP in product_SSPs]
+                product_stock_list = [product_SSP.stock for product_SSP in product_SSPs] 
+                product_size_list  = [product_SSP.size for product_SSP in product_SSPs]
+                goal_name_list     = [goal_product.name for goal_product in goals_product]
+                is_soldout         = bool(sum(product_stock_count_list) == 0)
+                product_info = {
+                        "id"          : product.id,
+                        "displayTitle": product.name, 
+                        "subTitle"    : product.sub_name,
+                        "imageUrl"    : product.image_set.get(is_main=True).image_url, 
+                        "symbolURL"   : goal_name_list,               
+                        "description" : product.description,
+                        "displayPrice": product_price_list,
+                        "displaySize" : product_size_list,
+                        "isNew"       : product.is_new,
+                        "isSoldout"   : is_soldout
+                        }
+                
+                result.append(product_info)
+            
+            return JsonResponse({"data": result, "message": "SUCCESS"}, status=200)
