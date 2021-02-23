7b526452970c042aaa0d1ffe1c26440986776d36
hyeyoonc <lucy8624@naver.com>
2021-02-24 00:39:53 +0900

    "Modify: coreof/urls.py Add product path"

--- coreof/urls.py
+++ coreof/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('user', include('user.urls')),
+    path('product'), include('product.urls'),
     path('order', include('order.urls')),
 ]
