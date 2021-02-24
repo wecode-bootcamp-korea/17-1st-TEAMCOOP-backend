7a1e2f93351ec2e6377093b3993281374e6abbc1
hyeyoonc <lucy8624@naver.com>
2021-02-25 01:29:48 +0900

    Modify: product/url.py 클래스 이름변경으로 url변경

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,8 @@
 from django.urls   import path
-from product.views import ProductView, ProductToCartView, ProductDetailView
+from product.views import ProductListView, ProductToCartView, ProductDetailView
 
 urlpatterns = [
-    path('', ProductView.as_view()),
+    path('', ProductListView.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
     path('/tocart', ProductToCartView.as_view()),
 ]
