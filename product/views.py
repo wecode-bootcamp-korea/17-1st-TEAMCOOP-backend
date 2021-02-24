4a1880bf2991cb974dff04f8b933542f60ecc821
hyeyoonc <lucy8624@naver.com>
2021-02-24 15:09:26 +0900

    "Modify: product models.py core/of/urls.py"

--- product/views.py
+++ product/views.py
@@ -29,40 +29,62 @@ from order.models   import Promotion, Order, Review, OrderProductStock, OrderSta
 
 class ProductView(View):
     def get(self, request, id=None):
-        result = []
-        # show_all
-        if id == None:
-            category_all = Category.objects.all()
-            for category in category_all: 
-                context = {}
-                context["id"] = category.id
-                context["subcategory"] = {}
-                context["subcategory"]["title"] = category.name
+        result            = []
+        context           = {}
+        product_info_list = []
+        product_info      = {}
+
+        sort     = request.GET.get('sort', all)
+        sort_dic = {}
+        sort_dic["lettervitamins"] = "Letter Vitamin"
+        sort_dic["minerals"]       = "Minerals"
+        sort_dic["herbs"]          = "Herbs"
+        sort_dic["probiotics"]     = "Probiotics"
+        sort_dic["collagens"]      = "Collagens"
+        sort_dic["proteins"]       = "Proteins"
+        sort_dic["boosts"]         = "Boosts"
+        sort_dic["immunity"]       = 1
+        sort_dic["brain"]          = 2
+        sort_dic["energy"]         = 3
+        sort_dic["eyes"]           = 4
+        sort_dic["heart"]          = 5
+        sort_dic["digestion"]      = 6
+        sort_dic["bones"]          = 7
+        sort_dic["fitness"]        = 8
+        sort_dic["new"]            = True
+        sort_dic["all"]            = ""
+
+        categories = [categories for categories in Category.objects.filter(name__contains=sort_dic[sort])]
+        if categories:
+            for category in categories: #category_all
+                context["id"]                         = category.id
+                context["subcategory"]                = {}
+                context["subcategory"]["title"]       = category.name
                 context["subcategory"]["description"] = category.description
                 
-                products_category = Product.objects.filter(category=category) 
-                product_info_list = []
-                for product_category in products_category:
-                    goals_product      = product_category.goal.all()   
-                    product_SSPs       = product_category.productstock_set.all()
-                    goal_name_list     = [goal_product.name for goal_product in goals_product]
-                    product_price_list = [product_SSP.price for product_SSP in product_SSPs]
-                    product_stock_list = [product_SSP.stock for product_SSP in product_SSPs]
-                    product_size_list  = [product_SSP.size for product_SSP in product_SSPs]
-                    is_soldout         = bool(sum(product_stock_list) == 0)
+                products = Product.objects.filter(category=category)    #products_category for product_category in products_category
+                
+               #product_info_list
+                for product in products:
+                    goals = product.goal.all()      #goals_product      for goal_product in goals_product
+                    product_stocks = product.productstock_set.all()     #product_SSPs     for product_SSP in product_SSPs 알아맞춰봥
+                    goal_name_list = [goal.name for goal in goals]
+                    product_stock_price_list = [product_stock.price for product_stock in product_stocks] #product_price_list
+                    product_stock_count_list = [product_stock.stock for product_stock in product_stocks] #product_stock_list
+                    product_stock_size_list  = [product_stock.size for product_stock in product_stocks]
+                    is_soldout = True if sum(product_stock_count_list) == 0 else False
 
-                    product_info = {
-                        "id"          : product_category.id,
-                        "displayTitle": product_category.name, 
-                        "subTitle"    : product_category.sub_name,
-                        "imageUrl"    : product_category.image_set.get(is_main=True).image_url,  
-                        "symbolURL"   : goal_name_list,             
-                        "description" : product_category.description,
-                        "displayPrice": product_price_list, 
-                        "displaySize" : product_size_list,
-                        "isNew"       : product_category.is_new,
-                        "isSoldout"   : is_soldout
-                    }
+                    product_info = {}#product_info
+                    product_info["id"]           = product.id
+                    product_info["displayTitle"] = product.name
+                    product_info["subTitle"]     = product.sub_name
+                    product_info["imageUrl"]     = product.image_set.get(is_main=True).image_url
+                    product_info["symbolURL"]    = goal_name_list
+                    product_info["description"]  = product.description
+                    product_info["displayPrice"] = product_stock_price_list
+                    product_info["displaySize"]  = product_stock_size_list
+                    product_info["isNew"]        = product.is_new
+                    product_info["isSoldout"]    = is_soldout
                     product_info_list.append(product_info)
 
                 context["item"] = product_info_list
@@ -70,109 +92,34 @@ class ProductView(View):
 
             return JsonResponse({"data": result, "message": "SUCCESS"}, status=200)
 
-        
-        #recently added  
-        if id == 99999:            
-            products = Product.objects.filter(is_new=True)
+        else:
+            products = Product.objects.filter(goal=sort_dic[sort]) or Product.objects.filter(is_new=sort_dic[sort])
+            #recently added                 # 프론트에 얘기하기 
+            #products = Product.objects.filter(is_new=True)
             for product in products:
-                goals_product = product.goal.all()
-                product_SSPs  = product.productstock_set.all()
+                goals = product.goal.all()      #goals_product      for goal_product in goals_product
+                product_stocks = product.productstock_set.all()     #product_SSPs     for product_SSP in product_SSPs 알아맞춰봥
+                goal_name_list = [goal.name for goal in goals]
+                product_stock_price_list = [product_stock.price for product_stock in product_stocks] #product_price_list
+                product_stock_count_list = [product_stock.stock for product_stock in product_stocks] #product_stock_list
+                product_stock_size_list  = [product_stock.size for product_stock in product_stocks]
+                is_soldout = True if sum(product_stock_count_list) == 0 else False
 
-                product_price_list = [product_SSP.price for product_SSP in product_SSPs]
-                product_stock_list = [product_SSP.stock for product_SSP in product_SSPs] 
-                product_size_list  = [product_SSP.size for product_SSP in product_SSPs]
-                goal_name_list     = [goal_product.name for goal_product in goals_product]
-                is_soldout         = bool(sum(product_stock_list) == 0)
-                product_info = {
-                        "id"          : product.id,
-                        "displayTitle": product.name, 
-                        "subTitle"    : product.sub_name,
-                        "imageUrl"    : product.image_set.get(is_main=True).image_url, 
-                        "symbolURL"   : goal_name_list,               
-                        "description" : product.description,
-                        "displayPrice": product_price_list,
-                        "displaySize" : product_size_list,
-                        "isNew"       : product.is_new,
-                        "isSoldout"   : is_soldout
-                        }
-                
-                result.append(product_info)
-            
-            return JsonResponse({"data": result, "message": "SUCCESS"}, status=200)
-
-        #카테고리별 상품보기
-        if Category.objects.filter(id=id).exists:
-            categories = Category.objects.filter(id=id)
-            for category in categories:
-                context = {}
-                context["id"] = category.id
-                context["subcategory"] = {}
-                context["subcategory"]["title"] = category.name
-                context["subcategory"]["description"] = category.description
-                
-                products_category = Product.objects.filter(category=category)
-                product_info_list = []
-                for product_category in products_category:
-                    goals_product = product.goal.all()
-                    product_SSPs  = product.productstock_set.all()
-
-                    product_price_list = [product_SSP.price for product_SSP in product_SSPs]
-                    product_stock_list = [product_SSP.stock for product_SSP in product_SSPs]
-                    product_size_list  = [product_SSP.size for product_SSP in product_SSPs]
-                    goal_name_list     = [goal_product.name for goal_product in goals_product]
-                    is_soldout         = bool(sum(product_stock_list) == 0)
-
-                    product_info = {
-                        "id"          : product_category.id,
-                        "displayTitle": product_category.name, 
-                        "subTitle"    : product_category.sub_name,
-                        "imageUrl"    : product_category.image_set.get(is_main=True).image_url,  
-                        "symbolURL"   : goal_name_list,              
-                        "description" : product_category.description,
-                        "displayPrice": product_price_list,
-                        "displaySize" : product_size_list,
-                        "isNew"       : product_category.is_new,
-                        "isSoldout"   : is_soldout
-                        }
-                    product_info_list.append(product_info)
+                product_info["id"]           = product.id
+                product_info["displayTitle"] = product.name
+                product_info["subTitle"]     = product.sub_name
+                product_info["imageUrl"]     = product.image_set.get(is_main=True).image_url
+                product_info["symbolURL"]    = goal_name_list
+                product_info["description"]  = product.description
+                product_info["displayPrice"] = product_stock_price_list
+                product_info["displaySize"]  = product_stock_size_list
+                product_info["isNew"]        = product.is_new
+                product_info["isSoldout"]    = is_soldout
+                product_info_list.append(product_info)
 
                 context["item"] = product_info_list
-                result.append(context)
-            
-            return JsonResponse({"data": result, "message": "SUCCESS"}, status=200)
-        
-class GoalView(View):
-    def get(self, request, id):
-        result = []
-        if Goal.objects.filter(id=id):
-            products = Product.objects.filter(goal=Goal.objects.get(id=id).id)
-            for product in products:
-                goals_product = product.goal.all()
-                product_SSPs = product.productstock_set.all()
-
-                product_price_list = [product_SSP.price for product_SSP in product_SSPs]
-                product_stock_list = [product_SSP.stock for product_SSP in product_SSPs]
-                product_size_list  = [product_SSP.size for product_SSP in product_SSPs]
-                goal_name_list     = [goal_product.name for goal_product in goals_product]
-                is_soldout         = bool(sum(product_stock_list) == 0)
-
-                product_info = {
-                        "id"           : product.id,
-                        "displayTitle" : product.name, 
-                        "subTitle"     : product.sub_name,
-                        "imageUrl"     : product.image_set.get(is_main=True).image_url,   #나중에 수정해야함 is_main
-                        "symbolURL"    : goal_name_list,            
-                        "description"  : product.description,
-                        "displayPrice" : product_price_list,
-                        "displaySize"  : product_size_list,
-                        "isNew"        : product.is_new,
-                        "isSoldout"    : is_soldout
-                        }
-                
-                result.append(product_info)
 
             return JsonResponse({"data": result, "message": "SUCCESS"}, status=200)
-
 
 class ProductDetailView(View):
     def get(self, request, product_id):
@@ -180,7 +127,7 @@ class ProductDetailView(View):
             return JsonResponse({'message': 'DOES_NOT_EXIST'}, status=404)
         product  = Product.objects.get(id=product_id)
         category = product.category.menu.name
-
+        # is_vegan, is_vegeterian
         vegan_level = product.vegan_level_id
         if vegan_level == 1:
             is_vegan      = True
@@ -191,21 +138,21 @@ class ProductDetailView(View):
         else:
             is_vegan      = False
             is_vegeterian = False
-
+        # size, price
         product_SSPs = ProductStock.objects.filter(product=product)  # SSP: size, stock, price
         if category == 'vitamins':
             price      = product_SSPs[0].price   
-            is_soldout = bool(product_SSPs[0].stock == 0)
+            is_soldout = True if product_SSPs[0].stock == 0 else False 
         else:
             price = {
                 product_SSPs[0].size : product_SSPs[0].price,
                 product_SSPs[1].size : product_SSPs[1].price
             }
             is_soldout = {
-                product_SSPs[0].size : bool(product_SSPs[0].stock == 0),
-                product_SSPs[1].size : bool(product_SSPs[1].stock == 0)
+                product_SSPs[0].size : True if product_SSPs[0].stock == 0 else False,
+                product_SSPs[1].size : True if product_SSPs[1].stock == 0 else False,
             }
-
+        # similar products
         similar_product_list = []
         goals_product        = product.goal.all()
         for goal_product in goals_product:
@@ -236,8 +183,42 @@ class ProductDetailView(View):
         context['dietaryHabitList']    = [dietary_habit.name for dietary_habit in product.dietary_habit.all()]
         context['similarProduct']      = similar_product_list[:2] 
         return JsonResponse({"data": context, "message": "SUCCESS"}, status=200)
+    
+class GoalView(View):
+    def get(self, request, id):
+        result = []
+        if Goal.objects.filter(id=id):
+            # product_result = []
+            products = Product.objects.filter(goal=Goal.objects.get(id=id).id)
+            for product in products:
+                goals = product.goal.all()
+                product_stocks = product.productstock_set.all()
 
-class ProductToCartView(View):  
+                product_stock_price_list = [product_stock.price for product_stock in product_stocks]
+                product_stock_count_list = [product_stock.stock for product_stock in product_stocks]
+                product_stock_size_list = [product_stock.size for product_stock in product_stocks]
+                is_soldout = True if sum(product_stock_count_list) == 0 else False
+
+                product_dic = {
+                        "id": product.id,
+                        "displayTitle": product.name, 
+                        "subTitle": product.sub_name,
+                        "imageUrl": product.image_set.get(is_main=True).image_url,   #나중에 수정해야함 is_main
+                        "symbolURL": [goal.name for goal in goals],#이름으로 변경                
+                        "description": product.description,
+                        "displayPrice": product_stock_price_list,
+                        "displaySize": product_stock_size_list,
+                        "isNew": product.is_new,
+                        "isSoldout": is_soldout
+                        }
+                
+                result.append(product_dic)
+
+            return JsonResponse({"data": result, "message": "SUCCESS"}, status=200)
+
+
+#Add 눌렀을 때 
+class ProductToCartView(View):
     @login_decorator
     def post(self, request):
             data          = json.loads(request.body)
@@ -245,33 +226,47 @@ class ProductToCartView(View):
             product_id    = data.get('productId', None)
             product_size  = data.get('productSize', None)
             product_price = data.get('productPrice', None)
+
             if not (product_id and product_price): 
                 return JsonResponse({"message": "KEY_ERROR"}, status=400)            
-            if not ProductStock.objects.filter(product_id=product_id, size=product_size):
-                return JsonResponse({"message": "PRODUCT_DOES_NOT_EXIST"}, status=400)
+
             product_price = float(product_price)
+
+            # orders insert
             if not Order.objects.filter(user=user, order_status_id=1).exists():
                 shipping_cost = 5 if product_price < 20 else 0
-                order_info    = Order.objects.create( 
+                order         = Order.objects.create(  # order_info
                     user            = user,
-                    order_number    = datetime.today().strftime("%Y%m%d") + str(randint(100000, 1000000)),
+                    order_number    = datetime.today().strftime("%Y%m%d") + str(randint(1000, 10000)),
                     order_status_id = 1,
                     sub_total_cost  = product_price,
                     shipping_cost   = shipping_cost,
                     total_cost      = product_price + shipping_cost
                 )
+                
             else:
-                order_info = Order.objects.get(user=user, order_status_id=1)
-                order_info.sub_total_cost = float(order_info.sub_total_cost) + product_price
-                order_info.shipping_cost  = 5 if order_info.sub_total_cost < 20 else 0
-                order_info.total_cost     = float(order_info.sub_total_cost) + order_info.shipping_cost
-                order_info.save()
+                order = Order.objects.get(user=user, order_status_id=1)
+                order.sub_total_cost = float(order.sub_total_cost) + product_price
+                order.shipping_cost  = 5 if order.sub_total_cost < 20 else 0
+                order.total_cost     = float(order.sub_total_cost) + order.shipping_cost
+                order.save()
+        
+            if not ProductStock.objects.filter(product_id=product_id, size=product_size):
+                return JsonResponse({"message": "PRODUCT_DOES_NOT_EXIST"}, status=400)
+
+            # order_product_stocks insert
             added_product = ProductStock.objects.get(product_id=product_id, size=product_size)
+            # OrderProductStock.objects.create(
+            #     order         = order,
+            #     product_stock = added_product,
+            #     quantity      = 1
+            # )
             OrderProductStock.objects.update_or_create(
-                order         = order_info,
+                order         = order,
                 product_stock = added_product,
                 defaults      = {
-                    "quantity" : 1
+                    'quantity' : 1
                 }
             )
+
             return JsonResponse({"message": "SUCCESS"}, status=200)
