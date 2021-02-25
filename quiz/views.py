8a7b083aa2fd0d0e6f3814d8921e737273eb0ea2
hyeyoonc <lucy8624@naver.com>
2021-02-25 13:56:39 +0900

    Modify: RecommendationView 수정

--- quiz/views.py
+++ quiz/views.py
@@ -5,25 +5,7 @@ from django.views     import View
 from django.db.models import Q
 
 from user.utils     import login_decorator
-from product.models import (Menu,
-                            Category, 
-                            Product,
-                            Image,
-                            ProductStock,
-                            RelatedProduct,
-                            Disease,
-                            ProductDisease,
-                            Allergy,
-                            ProductAllergy,
-                            Goal,
-                            ProductGoal,
-                            ProductDietaryHabit,
-                            DietaryHabit,
-                            VeganLevel,
-                            ActivityLevel,
-                            GenderCode,
-                            AgeLevel)
-from user.models    import User, Address
+from product.models import Product, Disease, Allergy, Goal
 from .models        import QuizResult 
 
 class RecommendationView(View):
@@ -43,41 +25,50 @@ class RecommendationView(View):
                 else:
                     one_answer_list = [key for key, value in answer.items() if value and key != 'id']
                 all_answer.append(one_answer_list)
-            print("========start============") 
-            print(all_answer)
+
+
             
-            gender = all_answer[1][0]
-            age = int(all_answer[2][0])
-            bmi = int(all_answer[3][0])
-            activity = all_answer[4][0]
-            care_smoker = all_answer[5][0]
+            gender       = all_answer[1][0]
+            age          = int(all_answer[2][0])
+            bmi          = int(all_answer[3][0])
+            activity     = all_answer[4][0]
+            care_smoker  = all_answer[5][0]
             care_drinker = all_answer[6][0]
+            goals        = all_answer[7]
+            vegan_type   = all_answer[8][0]
+            allergies    = all_answer[9]
+            diseases     = all_answer[10]
 
-            goals = all_answer[7]
-            goal_id_list = [goal.id for goal in Goal.objects.filter(name__in=goals)]
-            vegan_type = all_answer[8][0]
-            allergies = all_answer[9]
+            gender_code     = [1, 3] if gender == 'male' else [2, 3]
+            age_code        = [2, 3] if age >= 50 else [1, 3]
+            care_obesity    = [0, 1] if bmi >= 25 else [0]
+            activity_level  = [3] if activity == 'workoutMore6hours' else [1,2]
+            care_smoker     = bool(care_smoker == 'yesSmoke')
+            care_drinker    = [0, 1] if care_drinker == 'drinkingMore' else [0]
+            goal_id_list    = [goal.id for goal in Goal.objects.filter(name__in=goals)]
+            vegan_level     = {vegan_type == 'vegan': [1], vegan_type == 'vegetarian': [1, 2]}.get(True, [1, 2 ,3])
             allergy_id_list = [allergy.id for allergy in Allergy.objects.filter(name__in=allergies)]
-            diseases = all_answer[10]
             disease_id_list = [disease.id for disease in Disease.objects.filter(name__in=diseases)]
-
-            gender_code = [1, 3] if gender == 'male' else [2, 3] 
-            age_code = [1, 2] if age >= 50 else [1, 3] 
-            care_obesity = bool(bmi >= 25)
-            activity_level = [3] if activity == 'workoutMore6hours' else [1,2] 
-            care_smoker = bool(care_smoker == 'yesSmoke')
-            care_drinker = bool(care_drinker == 'drinkingMore')
-            vegan_level = {vegan_type == 'vegan': 1, vegan_type == 'vegetarian': 2}.get(True, 3)
             
-            q = Q(is_default=True)
-            q.add(Q(gender_code__in = gender_code) & Q(age_level__in = age_code) & Q(care_obesity = care_obesity) 
-                & Q(activity_level__in = activity_level) & Q(care_smoker = care_smoker) & Q(care_drinker = care_drinker) 
-                & Q(goal__in = goal_id_list) & Q(vegan_level = vegan_level) & ~Q(allergy__in = allergy_id_list) & Q(disease__in = disease_id_list), q.OR)
-            results_products = Product.objects.filter(q).distinct()
+            q = Q()
+            q.add(Q(gender_code__in = gender_code) & Q(age_level__in = age_code) 
+                & Q(care_obesity__in = care_obesity) & Q(activity_level__in = activity_level) 
+                & Q(care_smoker = care_smoker) & Q(care_drinker__in = care_drinker) 
+                & Q(goal__in = goal_id_list) & Q(vegan_level__in = vegan_level), q.OR)
+
+            if allergy_id_list:
+                q.add(~Q(allergy__in = allergy_id_list), q.AND)
+
+            if disease_id_list:
+                q.add(Q(disease__in = disease_id_list), q.AND)
+
+            results_products = Product.objects.filter(q | Q(is_default=True)).distinct()
+                      
+            if QuizResult.objects.filter(user=user):
+                user.recommendation.clear()
+            
             recommendations = []
             for result_product in results_products:
-                if QuizResult.objects.filter(user=user):
-                    user.recommendation.clear()
                 user.recommendation.add(result_product)
 
                 product_SSPs   = result_product.productstock_set.all()
@@ -99,7 +90,6 @@ class RecommendationView(View):
 
             return JsonResponse({'data':recommendations, 'message':'SUCCESS'}, status=200)
 
-            print(result_products)
-
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
