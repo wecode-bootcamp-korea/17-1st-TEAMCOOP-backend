07ae051d551fcb0f10230a3ac679cae60e18f9d9
yangahh <toaur6802@naver.com>
2021-02-26 03:26:42 +0900

    Modify: 에러 처리 추가, list comprehension 변경, order관련 URI 설정

--- order/urls.py
+++ order/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views      import CartView, CartDetailView, CheckOutView
 
 urlpatterns = [
-    path('/mycart', CartView.as_view()),
+    path('/cart', CartView.as_view()),
     path('/cart/<int:product_stock_id>', CartDetailView.as_view()),
     path('/checkout/<str:order_number>', CheckOutView.as_view()),
 ]
--- order/views.py
+++ order/views.py
@@ -1,157 +1,214 @@
 import json
-from datetime       import datetime
+from datetime               import datetime
 
-from django.http    import JsonResponse
-from django.views   import View
+from django.http            import JsonResponse
+from django.views           import View
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import MultipleObjectsReturned
 
-from user.utils     import login_decorator
-from user.models    import Address
-from product.models import ProductStock 
-from .models        import (
-    Order,
-    OrderProductStock,
-)
+from user.utils             import login_decorator
+from user.models            import Address
+from product.models         import ProductStock 
+from .models                import Order, OrderProductStock, OrderStatus
 
 class CartView(View):
-     @login_decorator
-     def get(self, request):
-        user = request.user
+    @login_decorator
+    def get(self, request):
+        try:
+            user = request.user
+            
+            if not Order.objects.filter(user=user, order_status=OrderStatus.objects.get(name='주문 전')).exists():
+                return JsonResponse({"message": "EMPTY"}, status=200)
+            
+            order          = Order.objects.get(user=user, order_status=OrderStatus.objects.get(name='주문 전'))
+            order_products = order.orderproductstock_set.filter(order=order)
         
-        if not Order.objects.filter(user=user, order_status_id=1).exists():
-            return JsonResponse({"message": "EMPTY"}, status=200)
-        
-        order_exist          = Order.objects.get(user=user, order_status_id=1)
-        order_products_exist = order_exist.orderproductstock_set.filter(order=order_exist)
-        
-        cart_product_list = []
-        for order_product_exist in order_products_exist:
-            product_SSP  = order_product_exist.product_stock 
-            product      = product_SSP.product
-            product_info = {
-                "category"        : product.category.menu.name,
-                "productId"       : product.id,
-                "productName"     : product.name,
-                "productSubName"  : product.sub_name,
-                "productStockId"  : product_SSP.id,
-                "productSize"     : product_SSP.size,
-                "productPrice"    : product_SSP.price,
-                "productStock"    : product_SSP.stock,
-                "productImageUrl" : product.image_set.get(is_main=True).image_url,
-                "productQuantity" : order_product_exist.quantity
+            cart_product_list = [{
+                "category"        : order_product.product_stock.product.category.menu.name,
+                "productId"       : order_product.product_stock.product.id,
+                "productName"     : order_product.product_stock.product.name,
+                "productSubName"  : order_product.product_stock.product.sub_name,
+                "productStockId"  : order_product.product_stock.id,
+                "productSize"     : order_product.product_stock.size,
+                "productPrice"    : order_product.product_stock.price,
+                "productStock"    : order_product.product_stock.stock,
+                "productImageUrl" : order_product.product_stock.product.image_set.get(is_main=True).image_url,
+                "productQuantity" : order_product.quantity
+                } for order_product in order_products
+            ]
+
+            data = {
+                "orderNumber": order.order_number,
+                "carts": cart_product_list,
             }
-            cart_product_list.append(product_info)
 
-        data = {
-            "orderNumber": order_exist.order_number,
-            "carts": cart_product_list,
-        }
+            return JsonResponse({"data": data, "message": "SUCCESS"}, status=200)
 
-        return JsonResponse({"data": data, "message": "SUCCESS"}, status=200)
+        except OrderStatus.DoesNotExist:
+            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=400)
+
+    @login_decorator
+    def post(self, request):
+        try:
+            data          = json.loads(request.body)
+            user          = request.user
+            product_id    = data['productId']
+            product_size  = data.get('productSize', None)
+            product_price = data['productPrice']
+
+            product_price = float(product_price)
+
+            if not Order.objects.filter(user=user, order_status=OrderStatus.objects.get(name='주문 전')).exists():
+                shipping_cost = 5 if product_price < 20 else 0
+                
+                order_info    = Order.objects.create( 
+                    user            = user,
+                    order_number    = datetime.today().strftime("%Y%m%d") + str(randint(10000, 100000)),
+                    order_status    = OrderStatus.objects.get(name='주문 전'),
+                    sub_total_cost  = product_price,
+                    shipping_cost   = shipping_cost,
+                    total_cost      = product_price + shipping_cost
+                )
+            else:
+                order_info = Order.objects.get(user=user, order_status_id=1)
+                order_info.sub_total_cost = float(order_info.sub_total_cost) + product_price
+                order_info.shipping_cost  = 5 if order_info.sub_total_cost < 20 else 0
+                order_info.total_cost     = float(order_info.sub_total_cost) + order_info.shipping_cost
+                order_info.save()
+
+            # order_product_stocks insert
+            added_product = ProductStock.objects.get(product_id=product_id, size=product_size)
+            OrderProductStock.objects.update_or_create(
+                order         = order_info,
+                product_stock = added_product,
+                defaults      = {
+                    "quantity" : 1
+                }
+            )
+
+            return JsonResponse({"message": "SUCCESS"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+        except OrderStatus.DoesNotExist:
+            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=400)
+        
+        except ProductStock.DoesNotExist:
+            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=400)
+
+
+    @login_decorator
+    def patch(self, request):
+        try:
+            data             = json.loads(request.body)
+            user             = request.user
+            product_id       = data['productId']
+            product_stock_id = data['productStockId']
+            quantity_new     = data['productQuantity']
+            size_new         = data.get('productSize', None)
+            
+            order = Order.objects.get(user=user, order_status=OrderStatus.objects.get(name='주문 전'))
+            
+            target_order_product      = OrderProductStock.objects.get(order=order, product_stock_id=product_stock_id) 
+            target_order_product_size = target_order_product.product_stock.size
+            product_new               = ProductStock.objects.get(product_id=product_id, size=size_new)
+
+            # 변경할 상품이 이미 장바구니에 존재하는 경우
+            if target_order_product_size != size_new:
+                if OrderProductStock.objects.filter(order=order, product_stock=product_new).exists(): 
+                    order_product_already_exist = OrderProductStock.objects.get(order=order, product_stock=product_new)
+
+                    if bool(product_new.stock - quantity_new < 0):
+                        return JsonResponse({"message": "OUT_OF_STOCK"}, status=200)
+
+                    order_product_already_exist.quantity = quantity_new
+                    order_product_already_exist.save()
+
+                    # 원래 있던 상품은 새로 바꿀 상품이 이미 존재하는게 있으므로 삭제 
+                    target_order_product.delete()
+                    return JsonResponse({"message": "SUCCESS"}, status=200)
+
+            # 변경할 상품이 장바구니에 없는 경우
+            if bool(product_new.stock - int(quantity_new) < 0):
+                return JsonResponse({"message": "OUT_OF_STOCK"}, status=200)
+
+            target_order_product.product_stock = product_new
+            target_order_product.quantity      = quantity_new
+            target_order_product.save()
+
+            return JsonResponse({"message": "SUCCESS"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+        
+        except Order.DoesNotExist:
+            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=400)
+
+        except ProductStock.DoesNotExist:
+            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=400)
+
+        except OrderStatus.DoesNotExist:
+            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=400)
+
+        except MultipleObjectsReturned:
+            return JsonResponse({"message": "MULTIPLE_OBJECTS_RETURNED"}, status=400)
 
 class CartDetailView(View):
     @login_decorator
-    def post(self, request, product_stock_id):
-        data         = json.loads(request.body)
-        user         = request.user
-        product_id   = data.get('productId', None)
-        size_new     = data.get('productSize', None)
-        quantity_new = data.get('productQuantity', None)
-        
-        if not (product_id and quantity_new):
-            return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-        if not ProductStock.objects.filter(id=product_stock_id):
-            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=404)
-
-        if not ProductStock.objects.filter(product_id=product_id, size=size_new):
-            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=400)
-        
-        order_exist          = Order.objects.get(user=user, order_status_id=1)
-        target_order_product = OrderProductStock.objects.filter(order=order_exist, product_stock_id=product_stock_id)
-        
-        # invalid product check
-        if not target_order_product:
-            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=400)
-        
-        target_order_product      = target_order_product[0] 
-        target_order_product_size = target_order_product.product_stock.size
-        product_new               = ProductStock.objects.get(product_id=product_id, size=size_new)
-
-        # 변경할 상품이 이미 OrderProductStock에 존재하는 경우
-        if target_order_product_size != size_new: 
-            order_product_already_exist = OrderProductStock.objects.filter(order=order_exist, product_stock=product_new) 
-
-            if order_product_already_exist:
-
-                if bool(product_new.stock - quantity_new < 0):
-                    return JsonResponse({"message": "OUT_OF_STOCK"}, status=200)
-
-                order_product_already_exist[0].quantity = quantity_new
-                order_product_already_exist[0].save()
-
-                # 원래 있던 상품은 새로 바꿀 상품이 이미 존재하는게 있으므로 삭제 
-                target_order_product.delete()
-                return JsonResponse({"message": "SUCCESS"}, status=200)
-
-        if bool(product_new.stock - int(quantity_new) < 0):
-            return JsonResponse({"message": "OUT_OF_STOCK"}, status=200)
-
-        target_order_product.product_stock = product_new
-        target_order_product.quantity      = quantity_new
-        target_order_product.save()
-
-        return JsonResponse({"message": "SUCCESS"}, status=200)
-
-    @login_decorator
     def delete(self, request, product_stock_id):
+        try:
+            if not ProductStock.objects.filter(id=product_stock_id):
+                return JsonResponse({"message": "DOES_NOT_EXIST"}, status=404)
 
-        if not ProductStock.objects.filter(id=product_stock_id):
-            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=404)
+            user          = request.user
+            order         = Order.objects.get(user=user, order_status=OrderStatus.objects.get(name='주문 전'))
+            order_product = order.product_stock.get(id=product_stock_id)
 
-        user          = request.user
-        order_exist   = Order.objects.get(user=user, order_status_id=1)
-        product_exist = ProductStock.objects.get(id=product_stock_id)
+            order_product.delete()
 
-        target_order_product = OrderProductStock.objects.filter(order=order_exist, product_stock=product_exist)
-        if not target_order_product:
-            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=404)
+            # 장바구니에 상품이 없을 경우 order 정보 삭제
+            if not OrderProductStock.objects.filter(order=order).exists():
+                order.delete()
 
-        target_order_product[0].delete()
+            return JsonResponse({"message": "SUCCESS"}, status=200) 
 
-        # 장바구니에 상품이 없을 경우 order 정보 삭제
-        if not OrderProductStock.objects.filter(order=order_exist):
-            order_exist.delete()
+        except Order.DoesNotExist:
+            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=400)
 
-        return JsonResponse({"message": "SUCCESS"}, status=200) 
+        except MultipleObjectsReturned:
+            return JsonResponse({"message": "MULTIPLE_OBJECTS_RETURNED"}, status=400)
+
+        except OrderStatus.DoesNotExist:
+            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=400)
+
+        except OrderProductStock.DoesNotExist:
+            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=400)
 
 class CheckOutView(View):
     @login_decorator
     def get(self, request, order_number):
         user = request.user
 
-        if not order_number:
-            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+        if not Order.objects.filter(order_number=order_number).exists():
+            return JsonResponse({"message": "DOES_NOT_EXIST"}, status=404)
 
         order_info     = Order.objects.get(order_number=order_number)
         order_products = order_info.orderproductstock_set.all()
-        
-        cart_product_list = []
-        for order_product in order_products:
-            product_SSP  = order_product.product_stock
-            product      = product_SSP.product
-            product_info = {
-                "category"        : product.category.menu.name,
-                "productId"       : product.id,
-                "productName"     : product.name,
-                "productSubName"  : product.sub_name,
-                "productStockId"  : product_SSP.id,
-                "productSize"     : product_SSP.size,
-                "productPrice"    : product_SSP.price,
-                "productIsSoldOut": bool(product_SSP.stock - order_product.quantity <= 0), 
-                "productImageUrl" : product.image_set.get(is_main=True).image_url,
-                "productQuantity" : order_product.quantity
-            }
-            cart_product_list.append(product_info)
+
+        cart_product_list = [{
+            "category"        : order_product.product_stock.product.category.menu.name,
+            "productId"       : order_product.product_stock.product.id,
+            "productName"     : order_product.product_stock.product.name,
+            "productSubName"  : order_product.product_stock.product.sub_name,
+            "productStockId"  : order_product.product_stock.id,
+            "productSize"     : order_product.product_stock.size,
+            "productPrice"    : order_product.product_stock.price,
+            "productIsSoldOut": bool(order_product.product_stock.stock - order_product.quantity <= 0),
+            "productImageUrl" : order_product.product_stock.product.image_set.get(is_main=True).image_url,
+            "productQuantity" : order_product.quantity
+            } for order_product in order_products
+        ]
          
         address_info = user.address_set.filter(is_main=True)
         address      = address_info[0].address if address_info else ""
@@ -169,37 +226,39 @@ class CheckOutView(View):
             "carts"      : cart_product_list,
             "user"       : user_info
         }
+
         return JsonResponse({"data": data, "message": "SUCCESS"}, status=200)
         
     @login_decorator
     def post(self, request, order_number):
-        data = json.loads(request.body)
-        sub_total_cost = data.get('subTotalCost', None)
-        shipping_cost  = data.get('shippingCost', None)
-        total_cost     = data.get('totalCost', None)
-        address_order  = data.get('address', None)
-        zipcode_order  = data.get('zipcode', None)
-        user           = request.user
+        try:
+            data = json.loads(request.body)
+            sub_total_cost = data['subTotalCost']
+            shipping_cost  = data['shippingCost']
+            total_cost     = data['totalCost']
+            address_order  = data['address']
+            zipcode_order  = data['zipcode']
+            user           = request.user
 
-        if not (sub_total_cost and str(shipping_cost) and total_cost and address_order and zipcode_order):
+            order_info = Order.objects.get(order_number=order_number)
+            order_info.order_status     = OrderStatus.objects.get(name='결제 완료')
+            order_info.sub_total_cost   = float(sub_total_cost)
+            order_info.shipping_cost    = float(shipping_cost)
+            order_info.total_cost       = float(total_cost)
+            order_info.save()
+
+            Address.objects.update_or_create(
+                user     = user,
+                address  = address_order,
+                zip_code = zipcode_order,
+                order    = order_info,
+                defaults = {
+                    'is_main': False
+                }
+            )
+
+            return JsonResponse({"message": "SUCCESS"}, status=200)
+
+        except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
-
-        order_info = Order.objects.get(order_number=order_number)
-        order_info.order_status_id = 2
-        order_info.sub_total_cost  = float(sub_total_cost)
-        order_info.shipping_cost   = float(shipping_cost)
-        order_info.total_cost      = float(total_cost)
-        order_info.save()
-
-        Address.objects.update_or_create(
-            user     = user,
-            address  = address_order,
-            zip_code = zipcode_order,
-            defaults = {
-                'order'  : order_info,
-                'is_main': False
-            }
-        )
-
-        return JsonResponse({"message": "SUCCESS"}, status=200)
         
