3796489ab8686db9a07d8ad979db63ba156692a1
hyeyoonc <lucy8624@naver.com>
2021-02-26 10:31:02 +0900

    Modify: ProductToCartView url 삭제

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,7 @@
 from django.urls   import path
-from product.views import ProductListView, ProductToCartView, ProductDetailView
+from product.views import ProductListView, ProductDetailView
 
 urlpatterns = [
     path('', ProductListView.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
-    path('/tocart', ProductToCartView.as_view()),
 ]
